cb18c535a622e250cbc51bc17fa6f4c821181498
Khorolskyi Vitalii <informer.dp@gmail.com>
2022-07-20 22:56:46 +0300

    modified counter

--- app/models/model_table.php
+++ app/models/model_table.php
@@ -2,6 +2,7 @@
 /*********Класс работы с таблицами***********/
 /*********WORKING WITH TABLES***********/
 require "model.php";
+require_once "config/functions.php";
 class Table extends Model{
   public function __construct(){
 
@@ -21,6 +22,15 @@ class Table extends Model{
 public static function get_row_details($table_name,$row_id){
   $pdo=connect_to_db();
   $sql="SELECT * FROM ".$table_name." WHERE `id`=:row_id";
+  $stmt=$pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  $stmt->execute(array('row_id' => $row_id));
+  foreach ($stmt as $row){
+    return $row;
+}
+}
+public static function row_update($table_name,$row_id,$params){
+  $pdo=connect_to_db();
+  $sql="UPDATE ".$table_name." SET".$params." WHERE `id`=:row_id";
   $stmt=$pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
   $stmt->execute(array('row_id' => $row_id));
   foreach ($stmt as $row){
--- includes/dashboard.php
+++ includes/dashboard.php
@@ -1,6 +1,18 @@
 <?php
 /**********dashboard************/
-
+function operation_count($operation){
+  $pdo=connect_to_db();
+  $operations = "SELECT COUNT(*) FROM operations WHERE `operation`=:operation ORDER BY `created` DESC";
+  $stmt=$pdo->prepare($operations, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  $stmt->execute(array('operation' => $operation));
+  $result['name']=get_name_by_id($pdo,"helper_operations",$operation);
+foreach($stmt as $k=>$v){
+  $result['counter']=$v;
+}
+return $result;
+}
   ?>
 <h2>Привет, Чирвочка!!!</h2><hr>
 <h3><a href="./?inc=operations_dates">Операции</a></h3>
+<?
+print_r(operation_count(7));
